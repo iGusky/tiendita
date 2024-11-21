6553ace760a09af9faf9cc9b9744b3c7c525fba6
Gustavo Hernández <gustavohernandez.dev@gmail.com>
2024-11-21 14:08:59 -0600

    migrated to mysql

--- database/migrations/2024_10_21_161342_create_products_table.php
+++ database/migrations/2024_10_21_161342_create_products_table.php
@@ -4,14 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('inventory.products', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->string('code', 20)->primary()->unique()->nullable(false);
             $table->string('description', 255)->nullable(false);
             $table->decimal('price', 8, 2)->nullable(false);
